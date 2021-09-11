f5da199127cd9bd687e4219b30ea678251ebd2ee
Sableuze <78899681+Sableuze@users.noreply.github.com>
2021-09-11 15:21:54 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+function hide(item) {
+    document.querySelector(item).style.display = 'none'
+    // if (window.innerWidth <= 500) {
+    //     document.querySelector('.popup__body').style.padding = '50px 0 30px'
+    // }
+}
+
+function show(item) {
+    document.querySelector(item).style.display = 'block'
+}
